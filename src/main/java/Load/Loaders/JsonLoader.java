da3b4fd4251f91d1be4d1e0be8c77fbff1e62c24
Shira Semo <shirasemo3@gmail.com>
2022-01-16 20:19:45 +0200

    save changes at sunday

--- src/main/java/Load/Loaders/JsonLoader.java
+++ src/main/java/Load/Loaders/JsonLoader.java
@@ -2,15 +2,27 @@ package Load.Loaders;
 
 import Load.Loader;
 import Transform.ItemAsMap;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class JsonLoader implements Loader {
     @Override
     public void load(ArrayList<ArrayList<ItemAsMap>> item, File[] dest) {
-        for (int i = 0; i < dest.length; i++) {
-            File to = dest[i];
-        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        for (int i = 0; i < item.size(); i++)
+            for (int j = 0; j < item.get(i).size(); j++) {
+                try {
+                    String input = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(item.get(i).get(j));
+                    objectMapper.writeValue(dest[i], input);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 }
